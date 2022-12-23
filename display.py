1e90df67139d21d1df9b154490ab2a22d157c3cb
Quentin <51925676+couaky@users.noreply.github.com>
2022-12-23 22:30:32 +0100

    Clean display

--- display.py
+++ display.py
@@ -8,29 +8,6 @@ from heatmap import HeatMap
 from utils import Vec2
 
 
-def heat(data, size):
-    heat_map_img = Image.new("HSV", (size, size))
-
-    heat_map_rgb = []
-    for line in data:
-        for pix in line:
-            pix_rgb = math.floor((1.0 - pix) * 170)
-            heat_map_rgb.append((pix_rgb, 255, 255))
-    heat_map_img.putdata(heat_map_rgb)
-
-    return heat_map_img
-
-def roads(data, size):
-    roads_map_img = Image.new("RGB", (size, size))
-
-    roads_map_rgb = []
-    for line in data:
-        for pix in line:
-            roads_map_rgb.append((pix, pix, pix))
-    roads_map_img.putdata(roads_map_rgb)
-
-    return roads_map_img
-
 class Printer:
     def __init__(self, world_settings: WorldSettings) -> None:
         self.world_settings = world_settings
